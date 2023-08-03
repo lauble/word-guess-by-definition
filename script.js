dceda7b31e3a4dce5c68abfcf6c661557d51b752
lauble <7139369-lauble@users.noreply.replit.com>
2023-08-03 14:58:34 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,97 @@
+import { words } from "./words.js";
+console.log(words)
+
+const keys = document.querySelectorAll(".kbd");
+const textbox = document.getElementById("textbox");
+const deleteButton = document.getElementById("delete-btn");
+const submitButton = document.getElementById("submit-btn");
+const clearButton = document.getElementById("clear-btn");
+const definitionContainer = document.getElementById("definition");
+
+// Function to fetch a random definition from the API
+async function fetchRandomDefinition() {
+	const word = "horse";
+	const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+
+	try {
+		const response = await fetch(url);
+		if (!response.ok) {
+			throw new Error("Failed to fetch definition");
+		}
+		const data = await response.json();
+		const definition = data[0]['meanings'][0]['definitions'][0]['definition'];
+		definitionContainer.innerHTML = definition.toLowerCase();
+	} catch (error) {
+		console.error(error);
+		return "Failed to fetch definition";
+	}
+}
+
+fetchRandomDefinition();
+
+function updateTextBox(key) {
+	textbox.innerHTML += key;
+}
+
+function handleKeyPress(event) {
+	const key = event.key.toLowerCase();
+	const kbdElement = document.querySelector(`kbd[data-key="${key}"]`);
+	if (kbdElement) {
+		kbdElement.classList.add("active");
+		updateTextBox(key);
+	}
+}
+
+function handleKeyRelease(event) {
+	const key = event.key.toLowerCase();
+	const kbdElement = document.querySelector(`kbd[data-key="${key}"]`);
+	if (kbdElement) {
+		kbdElement.classList.remove("active");
+	}
+}
+
+function addClickListenersToKeys() {
+	keys.forEach((key) => {
+		key.addEventListener("click", () => {
+			const keyLetter = key.textContent.toLowerCase();
+			key.classList.add("active");
+			updateTextBox(keyLetter);
+			setTimeout(() => {
+				key.classList.remove("active");
+			}, 100);
+		});
+	});
+}
+
+function handleDelete() {
+	const currentText = textbox.innerHTML;
+	if (currentText.length > 0) {
+		textbox.innerHTML = currentText.slice(0, -1); // Remove the last character
+	}
+}
+
+function handleClear() {
+	textbox.innerHTML = "";
+}
+
+keys.forEach((key) => {
+	const keyLetter = key.textContent.toLowerCase();
+	key.setAttribute("data-key", keyLetter);
+});
+
+// document event listeners
+document.addEventListener("keyup", handleKeyRelease);
+document.addEventListener("keydown", (event) => {
+	if (event.code === "Backspace") {
+		handleDelete();
+	} else {
+		handleKeyPress(event);
+	}
+});
+
+// button event listeners
+deleteButton.addEventListener("click", handleDelete);
+clearButton.addEventListener("click", handleClear);
+
+// Add click listeners for each key
+addClickListenersToKeys();
---
+++ words.js
@@ -0,0 +1,15 @@
+export const words = [
+	"boat",
+	"house",
+	"chase",
+	"hello",
+	"dog",
+	"cat",
+	"hover",
+	"box",
+	"ancient",
+	"marry",
+	"multiple",
+	"show"
+];
+
